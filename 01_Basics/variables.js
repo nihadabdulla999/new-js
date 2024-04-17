1aa2be3729dd1e879a609aea8d1f29a0608b7ba7
Nihadabdulla999 <49088916+nihadabdulla999@users.noreply.github.com>
2024-04-17 16:16:34 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,6 @@
+const accountID=12345
+let accountEmail="nihad@google.com"
+var accountPassword="2233"
+accountCity="bangalore"
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
